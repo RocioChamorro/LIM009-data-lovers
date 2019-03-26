35c32407e7f4b7784f971c260d426422b3f6deaf
RocioChamorro <chamorrorocio5@gmail.com>
2019-03-26 09:06:55 -0500

    Funcion de ordenado

--- src/data.js
+++ src/data.js
@@ -2,9 +2,147 @@
 
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
-
+/*
 const example = () => {
   return 'example';
 };
 
 window.example = example;
+*/
+//console.log(POKEMON.pokemon[0].name);
+//console.log(object.value(POKEMON.pokemon.));
+
+//listaPoke = document.getElementById("root").innerHTML;
+//let listaPoke = '';
+
+/*
+const funPoke=(data) => {
+  const listaPoke = document.getElementById("root");
+  for(let i=0; i< data.length;i++)
+  {
+    listaPoke.innerHTML += `<div>
+      <img src =${data[i].img} />
+      <div>
+        <p>${data[i].name}</p>  
+      </div>
+    </div>
+ `; 
+  }
+}
+
+console.log(funPoke(POKEMON.pokemon));
+*/
+/*
+
+  for(let i=0; i< POKEMON.pokemon.length;i++)
+  {
+    let listaPoke += POKEMON.pokemon[i];
+   
+
+  }
+console.log(listaPoke);
+*/
+
+
+
+
+
+/*
+for(let i=0; i<POKEMON.pokemon.length; i++){
+  const arrProperPoke=Object.entries(POKEMON.pokemon[i])
+  console.log(arrProperPoke[2]);
+  // console.log(POKEMON.pokemon[i][arrProperPoke[i]]);
+}
+
+
+
+*/
+
+
+
+
+//document.getElementById("p").innerHTML=listaPoke;
+
+
+
+
+
+
+
+
+
+
+/*
+
+const boxPoke = () => {
+for(i=0; i< POKEMON.pokemon.length;i++){
+  box.innerHTML +=
+  <div>
+
+  </div>
+}
+}
+*/
+/*
+const conteinerPoke =
+document.getElementById("root");
+const infoPoke = document.getElementById("");
+
+let string = "";
+for(let i=0; i<POKEMON.pokemon.length; i++){
+  string += `
+  <input type="radio" name="gender" value=${arrNamesProperty[i]}>${arrNamesProperty[i]}<br> `
+}
+*/
+
+/*
+const funPoke=(data) => {
+  const listaPoke = document.getElementById("root");
+  for(let i=0; i< data.length;i++)
+  { 
+  
+    listaPoke.innerHTML += `<div>
+      <img src =${data[i].img} />
+      <div>
+        <p>${data[i].name}</p>  
+      </div>
+    </div>
+ `; 
+  }
+}
+console.log(funPoke(POKEMON.pokemon));
+*/
+let arrNom = [];
+for(let i=0; i<POKEMON.pokemon.length; i++){
+  arrNom.push(POKEMON.pokemon[i].name);
+}
+
+
+function ordenar(arr){
+if(document.getElementById("myBtn").value === "A-Z"){
+  arr.sort();
+}
+else if(document.getElementById("myBtna").value === "Z-A"){
+  arr.sort();
+  arr.reverse();
+}
+return arr;
+}
+//ordenar(arrNom);
+console.log(ordenar(arrNom));
+document.getElementById("demo").innerHTML=arrNom
+/*
+
+
+let dataOrder = [];
+for(let i of arr){
+dataOrder = arr.sort();
+};
+console.log(dataOrder);
+
+let dataReverse = [];
+for(let i of arr){
+dataReverse = arr.reverse(arr[i]);
+};
+console.log(dataReverse);
+*/
