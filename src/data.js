0ed90b5010a6439e9ad6394cddc49f54c5390852
RocioChamorro <chamorrorocio5@gmail.com>
2019-03-27 09:18:13 -0500

    Funcion de ordenado A-Z y Z-A

--- src/data.js
+++ src/data.js
@@ -100,7 +100,6 @@ const funPoke=(data) => {
   const listaPoke = document.getElementById("root");
   for(let i=0; i< data.length;i++)
   { 
-  
     listaPoke.innerHTML += `<div>
       <img src =${data[i].img} />
       <div>
@@ -114,26 +113,51 @@ console.log(funPoke(POKEMON.pokemon));
 */
 let arrNom = [];
 for(let i=0; i<POKEMON.pokemon.length; i++){
-  arrNom.push(POKEMON.pokemon[i].name);
+  arrNom.push(`<div>
+  <div>
+  <p>${POKEMON.pokemon[i].name}<p/>
+  </div>
+  <div>
+    <img src =${POKEMON.pokemon[i].img} />
+  </div><<
+  
+</div>`);
+}
+const botonAZ = document.getElementById("A-Z");
+const botonZA = document.getElementById("Z-A");
+const ListaOrde = document.getElementById("demo");
+botonAZ.addEventListener("click", ()=>{
+  const resultado = ordenarAZ(arrNom);
+ListaOrde.innerHTML=resultado;
+});
+botonZA.addEventListener("click", ()=> {
+  const resultado = ordenarZA(arrNom);
+ListaOrde.innerHTML=resultado; 
+});
+
+function ordenarAZ(arr){
+if(botonAZ.value === "A-Z"){
+  arr.sort();
+}
+ return arr;
 }
 
 
-function ordenar(arr){
-if(document.getElementById("myBtn").value === "A-Z"){
-  arr.sort();
-}
-else if(document.getElementById("myBtna").value === "Z-A"){
-  arr.sort();
-  arr.reverse();
-}
-return arr;
+function ordenarZA(arr){
+  if(botonZA.value === "Z-A"){
+    arr.sort();
+    arr.reverse();
+  }
+ 
+  return arr;
 }
 //ordenar(arrNom);
-console.log(ordenar(arrNom));
-document.getElementById("demo").innerHTML=arrNom
+//console.log(ordenar(arrNom));
+
+
+
+
 /*
-
-
 let dataOrder = [];
 for(let i of arr){
 dataOrder = arr.sort();
