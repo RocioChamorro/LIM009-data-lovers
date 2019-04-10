80219de88538b95a86b43a201b2a2f14b5e9767f
Tu-username= <ivonne.morales.alarcon@gmail.com>
2019-04-10 11:22:20 -0500

    Subiendo carpeta img

--- src/data.js
+++ src/data.js
@@ -89,14 +89,3 @@ const imgEvolucion=(arr, ValueSelect)=>{
 } 
  
 
-   
-
-// esta es una función de ejemplo
-// puedes ver como agregamos la función a nuestro objeto global window
-/*
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
-*/
